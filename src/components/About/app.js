993da27d28a91ce95b8b9cd8adf6439318983d43
yashpratapkanthriya <32524804+yashpratapkanthriya@users.noreply.github.com>
2018-04-19 15:59:25 +0530

    initial commit

--- src/components/About/app.js
+++ src/components/About/app.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Switch,} from 'react-router-dom'
-
 import {hot} from 'react-hot-loader'
 
 
@@ -12,11 +11,25 @@ import '../../css/lato-web.css'
 import '../../css/poppins.css'
 import '../../css/app.css'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap'
+import 'popper.js/dist/umd/popper'
+
 
 const HomePage = (props) => {
     return(
     <div className="container">
         React App
+        <div className="dropdown">
+            <button type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                Dropdown button
+            </button>
+            <div className="dropdown-menu">
+                <a className="dropdown-item" href="#">Link 1</a>
+                <a className="dropdown-item" href="#">Link 2</a>
+                <a className="dropdown-item" href="#">Link 3</a>
+            </div>
+        </div>
     </div>
     )
 }
@@ -34,17 +47,6 @@ const App = (props) => (
     </Router>
 
 )
-/*const Ax = () => {
-    return (
-        <div>React</div>
-    )
-}
-const App = (props) => {
-    console.log("Running...")
-    return (
 
-        <div><Ax/>hello</div>
-    )
-}*/
 
 export default hot(module)(App)
--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const webpack = require('webpack');
 
 module.exports = {
     entry: {
@@ -12,7 +12,24 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: 'iAuthNet-Dev',
             template: './dist/index.html'
-        })
+        })/*,
+        new webpack.ProvidePlugin({
+            jQuery: 'jquery',
+            $: 'jquery',
+            jquery: 'jquery',
+            Popper: ['popper.js', 'default'],
+            Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
+            Button: 'exports-loader?Button!bootstrap/js/dist/button',
+            Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
+            Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
+            Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
+            Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
+            Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
+            Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
+            Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
+            Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+            Util: 'exports-loader?Util!bootstrap/js/dist/util'
+        })*/
     ],
     output: {
         filename: '[name].bundle.js',
@@ -35,15 +52,30 @@ module.exports = {
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use: [
-                    'file-loader'
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name : 'fonts/[name]-[hash:8].[ext]'
+                        }
+                    }
                 ]
             },
             {
-                test: /\.(png|svg|jpg|gif)$/,
+                test: /\.(jpg|png|gif|svg|jpeg|ico)$/,
                 use: [
-                    'file-loader'
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'images/[name].[ext]'
+                        },
+                    },
                 ]
+            },
+            {
+                test: /bootstrap\/dist\/js\/umd\//,
+                use: 'imports-loader?jQuery=jquery'
             }
         ]
     }
+
 };
